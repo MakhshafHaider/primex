6b53bd92a6d7d739a8a1c7b17194f40334cf80c0
Syed Makhshaf Haider <makhshafzaidi@gmail.com>
2023-02-15 12:49:52 +0500

    first commit

---
+++ mail/email.php
@@ -0,0 +1,20 @@
+
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$number = $_POST['phone'];
+
+$to = "makhshafzaidi@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n  Number = " . $number . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
